3a942a334de9f7aa63faa782934f0d342ea2bfcc
Marcin Przepiorowski <marcin@delphix.com>
2020-08-26 17:22:55 +0100

    using shared

--- vars/add_environment.groovy
+++ vars/add_environment.groovy
@@ -1,5 +1,5 @@
 def call(Map config=[:]) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    sh '/dxtoolkit/dx_create_env -envname "source" -envtype unix -host source -username "oracle" -authtype password -password oracle -toolkitdir "/home/oracle/toolkit" '
+    sh '/dxtoolkit/dx_create_env -configfile ${env.WORKSPACE}/dxtools.conf -envname "source" -envtype unix -host source -username "oracle" -authtype password -password oracle -toolkitdir "/home/oracle/toolkit" '
 }
