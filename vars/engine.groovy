8624aab933ab6389ae66bf712c5fb9c3da752f05
Marcin Przepiorowski <marcin@delphix.com>
2020-08-26 17:16:17 +0100

    using shared

--- vars/engine.groovy
+++ vars/engine.groovy
@@ -2,7 +2,7 @@ def call() {
     environment {
         DXTOOLKIT_CONF = "${env.WORKSPACE}/dxtools.conf"
     }
-    echo ${env.WORKSPACE}
+    echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL} ${env.WORKSPACE}"
     sh '/dxtoolkit/dx_config -convert todxconf -text hostname,$DLPX_ENGINE,80,$DLPX_USER,$DLPX_PASSWORD,true,http -configfile dxtools.conf'
     sh '/dxtoolkit/dx_get_appliance'
 }
