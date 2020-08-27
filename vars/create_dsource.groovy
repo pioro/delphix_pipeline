8e2bf680f76d9ebbe26d345a303e94f6a6cadf63
Marcin Przepiorowski <marcin@delphix.com>
2020-08-27 13:18:06 +0100

    using shared

---
+++ vars/create_dsource.groovy
@@ -0,0 +1,8 @@
+def call(Map config=[:]) {
+    try {
+      sh "dx_ctl_dsource -d 603 -action create -group "Untitled" -creategroup -dsourcename "${config.dbname}"  -type oracle -sourcename "${config.dbname}" -sourceinst "/u01/app/oracle/product/19c/db1" -sourceenv "${config.env}" -source_os_user "oracle" -dbuser delphixdb -password delphixdb -logsync yes"
+    } catch (Exception e) {
+         echo "Caught ${e.toString()}"
+         echo "Stage failed, but we continue"  
+    } 
+}
