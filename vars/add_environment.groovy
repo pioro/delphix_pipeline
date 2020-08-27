7ee800f1abb2a1616251d7676812f0a785f23d30
Marcin Przepiorowski <marcin@delphix.com>
2020-08-27 12:53:58 +0100

    using shared

--- vars/add_environment.groovy
+++ vars/add_environment.groovy
@@ -1,5 +1,10 @@
 def call(Map config=[:]) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    sh "/dxtoolkit/dx_create_env -envname ${config.name} -envtype unix -host ${config.host} -username '${config.username}' -authtype password -password '${config.password}' -toolkitdir '/home/oracle/toolkit' "
+    try {
+      sh "/dxtoolkit/dx_create_env -envname ${config.name} -envtype unix -host ${config.host} -username '${config.username}' -authtype password -password '${config.password}' -toolkitdir '/home/oracle/toolkit' "
+    } catch (Exception e) {
+         echo "Caught ${e.toString()}"
+         echo "Stage failed, but we continue"  
+    } 
 }
