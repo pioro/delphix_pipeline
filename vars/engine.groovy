f6b17f928c8018971fc431efe4bbdd06a456a861
Marcin Przepiorowski <marcin@delphix.com>
2022-01-05 12:49:25 +0000

    engine with agent

--- vars/engine.groovy
+++ vars/engine.groovy
@@ -1,20 +1,11 @@
 def call() {
-    pipeline {
-        agent {
-            docker {
-                    image 'pioro/dxtoolkit:latest'
-                    args '-u root -w /dxtoolkit -v ${WORKSPACE}:/config'
-                    reuseNode true
-            }
-        }
-        stages {
+    script {
+        /* Requires the Docker Pipeline plugin to be installed */
+        docker.image('pioro/dxtoolkit:latest').inside('-u root -w /dxtoolkit -v ${WORKSPACE}:/config') {
             stage('Configure connection to engine') {
-                steps {
-                    sh '/dxtoolkit/dx_config -convert todxconf -text hostname,$DLPX_ENGINE,80,$DLPX_USER,$DLPX_PASSWORD,true,http -configfile /config/dxtools.conf'
-                    sh '/dxtoolkit/dx_get_appliance -configfile /config/dxtools.conf'
-                }
+                sh '/dxtoolkit/dx_config -convert todxconf -text hostname,$DLPX_ENGINE,80,$DLPX_USER,$DLPX_PASSWORD,true,http -configfile /config/dxtools.conf'
+                sh '/dxtoolkit/dx_get_appliance -configfile /config/dxtools.conf'
             }
         }
     }
 }
-
