314ef0796499d0ca8362f0f574888e1ada92a2a1
Marcin Przepiorowski <marcin@delphix.com>
2023-03-08 13:05:50 +0100

    conditions

--- vars/prepare_vdb.groovy
+++ vars/prepare_vdb.groovy
@@ -5,6 +5,12 @@ def call(Map config=[:]) {
             stage('Preparing VDB') {
                 sh "/dxtoolkit/dx_provision_vdb -sourcename db19 -dbname '${config.dbname}' -group 'Oracle 19c Virtual Databases' -targetname ${config.dbname} -creategroup -environment singlenode -type oracle -envinst '/u01/app/oracle/product/19.0.0/dbhome_1'"
             }
+            stage("Set dataset") {
+                when { expression { ${config.bookmark} != 'full' } }
+                steps {
+                    sh "/dxtoolkit/dx_ctl_js_container -action restore -timestamp 1pctdata  -container_name $DLPX_CONTAINER -template_name $DLPX_TEMPLATE"
+                }
+            }
         }
     }
 }
