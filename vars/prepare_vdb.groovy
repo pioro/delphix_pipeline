8e480f7d03d818b2021388c529dd2e116f16b40b
Marcin Przepiorowski <marcin@delphix.com>
2023-03-08 09:26:44 +0100

    provision

--- vars/prepare_vdb.groovy
+++ vars/prepare_vdb.groovy
@@ -3,8 +3,9 @@ def call(Map config=[:]) {
         /* Requires the Docker Pipeline plugin to be installed */
         docker.image('pioro/dxtoolkit:latest').inside('-u root -w /dxtoolkit -v ${WORKSPACE}:/config') {
             stage('Preparing VDB') {
-                sh "/dxtoolkit/dx_refresh_db -name '${config.dbname}' "
+                sh "/dxtoolkit/dx_provision_vdb -d myengine -sourcename db19 -dbname '${config.dbname}' -targetname ${config.dbname} -creategroup -environment singlenode -type oracle -envinst '/u01/app/oracle/product/19.0.0/dbhome_1'"
             }
         }
     }
 }
+
