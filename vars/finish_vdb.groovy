999b95470b32097c064bad2d4133189f3629dc9a
Marcin Przepiorowski <marcin@delphix.com>
2023-03-09 12:38:29 +0100

    finish

--- vars/finish_vdb.groovy
+++ vars/finish_vdb.groovy
@@ -2,9 +2,7 @@ def call(Map config=[:]) {
     script {
         /* Requires the Docker Pipeline plugin to be installed */
         docker.image('pioro/dxtoolkit:latest').inside('-u root -w /dxtoolkit -v ${WORKSPACE}:/config') {
-            steps {
-                sh "/dxtoolkit/dx_ctl_db -name '${config.dbname}' -action disable"
-            }
+            sh "/dxtoolkit/dx_ctl_db -name '${config.dbname}' -action disable"
         }
     }
 }
