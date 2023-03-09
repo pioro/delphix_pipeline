a47b59cca6e735a1b7dfdd3d76c536d2e2e0c73f
Marcin Przepiorowski <marcin@delphix.com>
2023-03-09 10:30:50 +0100

    fix dsource

--- vars/create_dsource.groovy
+++ vars/create_dsource.groovy
@@ -3,7 +3,7 @@ def call(Map config=[:]) {
       script {
         /* Requires the Docker Pipeline plugin to be installed */
         docker.image('pioro/dxtoolkit:latest').inside('-u root -w /dxtoolkit -v ${WORKSPACE}:/config') {
-          sh "/dxtoolkit/dx_ctl_dsource -type oracle -sourcename '${config.env}' -sourceinst /u01/app/oracle/product/19.0.0/dbhome_1 -sourceenv '${config.dbname}' -source_os_user delphix -dbuser delphix -password delphix -group 'Oracle 19c Sources' -logsync yes -creategroup -dsourcename ${config.dbname} -action create"
+          sh "/dxtoolkit/dx_ctl_dsource -type oracle -sourcename '${config.dbname}' -sourceinst /u01/app/oracle/product/19.0.0/dbhome_1 -sourceenv '${config.env}' -source_os_user delphix -dbuser delphix -password delphix -group 'Oracle 19c Sources' -logsync yes -creategroup -dsourcename ${config.dbname} -action create"
         }
       }
     } catch (Exception e) {
