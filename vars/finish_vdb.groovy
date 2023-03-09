751cad6e81851426a84c0fdacbda7005fd508476
Marcin Przepiorowski <marcin@delphix.com>
2023-03-09 12:02:11 +0100

    pipeline fix

--- vars/finish_vdb.groovy
+++ vars/finish_vdb.groovy
@@ -2,8 +2,8 @@ def call(Map config=[:]) {
     script {
         /* Requires the Docker Pipeline plugin to be installed */
         docker.image('pioro/dxtoolkit:latest').inside('-u root -w /dxtoolkit -v ${WORKSPACE}:/config') {
-            stage('Finishing VDB') {
-                sh "/dxtoolkit/dx_ctl_vdb -name '${config.dbname}' -action disable"
+            steps {
+                sh "/dxtoolkit/dx_ctl_db -name '${config.dbname}' -action disable"
             }
         }
     }
