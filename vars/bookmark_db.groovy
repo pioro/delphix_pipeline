d7c441cd74945b6812490cba805b1618dcce3f93
Marcin Przepiorowski <marcin@delphix.com>
2023-03-10 09:35:54 +0100

    description fix

--- vars/bookmark_db.groovy
+++ vars/bookmark_db.groovy
@@ -2,10 +2,8 @@ def call(Map config=[:]) {
     script {
         /* Requires the Docker Pipeline plugin to be installed */
         docker.image('pioro/dxtoolkit:latest').inside('-u root -w /dxtoolkit -v ${WORKSPACE}:/config') {
-            stage('Preparing VDB') {
-                sh "/dxtoolkit/dx_snapshot_db -name '${config.dbname}' -group 'Oracle 19c Virtual Databases'" 
-                sh "/dxtoolkit/dx_ctl_bookmarks -name '${config.bookmarkname}' -dbname '${config.dbname}' -action create -timestamp latest"
-            }
+            sh "/dxtoolkit/dx_snapshot_db -name '${config.dbname}' -group 'Oracle 19c Virtual Databases'" 
+            sh "/dxtoolkit/dx_ctl_bookmarks -name '${config.bookmarkname}' -dbname '${config.dbname}' -action create -timestamp latest"
         }
     }
 }
